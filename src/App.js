60a93a13c2cbf8e18dd91ea4e1d243281e0bc106
shubh <shubhkeshari48@gmail.com>
2023-03-14 01:49:02 +0530

    com3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          shubh's first app udated
+          shubh's first version3
         </p>
         <a
           className="App-link"
