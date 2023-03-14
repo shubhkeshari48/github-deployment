00388175f351d4025af2c0aa1f890264414bfe98
shubh <shubhkeshari48@gmail.com>
2023-03-15 00:39:49 +0530

    component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Component from './Component';
+import Component2 from './Component2';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          shubh's first version4
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <Component/>
+      <Component/>
+      <Component/>
+      <Component/>
+      <Component2/>
+      
     </div>
   );
 }
---
+++ src/Component2.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Component2() {
+  return (
+    <div>Shubh is a developer</div>
+  )
+}
+
